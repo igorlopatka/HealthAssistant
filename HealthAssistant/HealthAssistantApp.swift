2b754d4ed8262c29ba23c5f5fa9611ef33cd2aab
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2023-09-05 13:00:39 +0200

    Initial HealthKit integration

--- HealthAssistant/HealthAssistantApp.swift
+++ HealthAssistant/HealthAssistantApp.swift
@@ -6,14 +6,43 @@
 //
 
 import SwiftUI
+import HealthKit
 
 @main
-struct HealthAssistantApp: App {
-
+struct HealthkitIntegrationApp: App {
+    
+    private let healthStore: HKHealthStore
+    
+    init() {
+        guard HKHealthStore.isHealthDataAvailable() else {  fatalError("This app requires a device that supports HealthKit") }
+        healthStore = HKHealthStore()
+        requestHealthkitPermissions()
+    }
+    
+    
+    private func requestHealthkitPermissions() {
+        let sampleTypesToShare = Set([
+            HKObjectType.quantityType(forIdentifier: .heartRate)!,
+            HKObjectType.quantityType(forIdentifier: .stepCount)!,
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!,
+        ])
+        
+        let sampleTypesToRead = Set([
+            HKObjectType.quantityType(forIdentifier: .heartRate)!,
+            HKObjectType.quantityType(forIdentifier: .stepCount)!,
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!,
+        ])
+        
+        healthStore.requestAuthorization(toShare: sampleTypesToShare, read: sampleTypesToRead) { (success, error) in
+            print("Request Authorization -- Success: ", success, " Error: ", error ?? "nil")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-
+            ContentView().environmentObject(healthStore)
         }
     }
 }
+
+extension HKHealthStore: ObservableObject{}
